c778a5acf94b86829e7397052f6981f7c54373d0
Jacob Grahn <jacob@grahn.io>
2024-08-11 17:06:29 -0400

    update date function

--- mend-hub/pr2/backup.sh
+++ mend-hub/pr2/backup.sh
@@ -10,11 +10,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 mysql_user='root'
 mysql_pass=$(yq -r -e .services.db.environment.MYSQL_ROOT_PASSWORD $SCRIPT_DIR/pr2.secrets.yaml)
 backup_path="/backups/pr2-db-backups"
-backup_file=$(printf -v date '%(%Y-%m-%d)T\n' -1).sql
+backup_file=$(date '+%Y-%m-%d').sql
 retention_days='30'
 
 # Create backup
-docker exec pr2-db-1 sh -c "exec mysqldump --all-databases -u'$mysql_user' -p'$mysql_pass'" > "$backup_path/$backup_file"
+docker exec pr2-db-1 sh -c "exec mysqldump --all-databases -u$mysql_user -p\"$mysql_pass\"" > "$backup_path/$backup_file"
 
 # Delete backups older than 30 days
 find $backup_path -type f -mtime +"$retention_days" -exec rm -f {} \;
