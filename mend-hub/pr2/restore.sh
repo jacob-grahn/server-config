5d11eb25f990e47a4b6d622c1b6c02d38f465818
Jacob Grahn <jacob@grahn.io>
2024-08-11 17:44:44 -0400

    restore prompts for backup

--- mend-hub/pr2/restore.sh
+++ mend-hub/pr2/restore.sh
@@ -9,6 +9,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Fetch credentials
 mysql_user='root'
 mysql_pass=$(yq -r -e .services.db.environment.MYSQL_ROOT_PASSWORD $SCRIPT_DIR/pr2.secrets.yaml)
+backup_path="/backups/pr2-db-backups"
+
+# Ask which version to restore
+echo "Available backups: $(ls $backup_path)"
+read -p 'Backup file: ' backup_file
 
 # Restore backup
-docker exec -i pr2-db-1 sh -c "exec mysql -u'$mysql_user' -p'$mysql_pass'" < /backups/pr2-db-backups/latest.sql
+docker exec -i pr2-db-1 sh -c "exec mysql -u$mysql_user -p\"$mysql_pass\"" < "$backup_path/$backup_file"
