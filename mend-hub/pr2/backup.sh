c8e7aee27a3591b38f4872a49d74e49df79aafd6
Jacob Grahn <jacob@grahn.io>
2024-08-11 11:37:01 -0400

    keep x days of backups

--- mend-hub/pr2/backup.sh
+++ mend-hub/pr2/backup.sh
@@ -9,6 +9,12 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Fetch credentials
 mysql_user='root'
 mysql_pass=$(yq $SCRIPT_DIR/pr2.secrets.yaml services.db.envireonment.MYSQL_ROOT_PASSWORD)
+backup_path="/backups/pr2-db"
+backup_file=$(printf -v date '%(%Y-%m-%d)T\n' -1).sql
+retention_days='30'
 
 # Create backup
-docker exec pr2-db sh -c "exec mysqldump --all-databases -u '$mysql_user' -p '$mysql_password'" > /backups/pr2-db/latest.sql
+docker exec pr2-db sh -c "exec mysqldump --all-databases -u '$mysql_user' -p '$mysql_password'" > "$backup_path/$backup_file"
+
+# Delete backups older than 30 days
+find $backup_path -type f -mtime +"$retention_days" -exec rm -f {} \;
--- mend-hub/scripts/setup.sh
+++ mend-hub/scripts/setup.sh
@@ -65,6 +65,7 @@ git clone https://github.com/jacob-grahn/server-config.git ~/server-config
 echo "Setting up cron job..."
 (crontab -l 2>/dev/null; echo "*/10 * * * * /home/fred-bot/server-config/mend-hub/scripts/sync.sh") | crontab -
 (crontab -l 2>/dev/null; echo "@reboot /home/fred-bot/server-config/mend-hub/scripts/sync.sh") | crontab -
+(crontab -l 2>/dev/null; echo "5 4 * * * /home/fred-bot/server-config/mend-hub/pr2/backup.sh") | crontab -
 EOF
 
 # Set up a root cron job to restart the VM once per month
