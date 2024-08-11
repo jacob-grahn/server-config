502f18d0d9d5f429c8c308e7e71c9577cd0553c8
Jacob Grahn <jacob@grahn.io>
2024-08-11 11:49:56 -0400

    update container name

--- mend-hub/pr2/restore.sh
+++ mend-hub/pr2/restore.sh
@@ -11,4 +11,4 @@ mysql_user='root'
 mysql_pass=$(yq -e .services.db.envireonment.MYSQL_ROOT_PASSWORD $SCRIPT_DIR/pr2.secrets.yaml)
 
 # Restore backup
-docker exec -i pr2-db sh -c "exec mysql -u '$mysql_user' -p '$mysql_pass'" < /backups/pr2-db/latest.sql
+docker exec -i pr2-db-1 sh -c "exec mysql -u '$mysql_user' -p '$mysql_pass'" < /backups/pr2-db/latest.sql
