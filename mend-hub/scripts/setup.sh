8a263b2c079ac3ab15343415665850186d6a4b42
Jacob Grahn <jacob@grahn.io>
2024-08-11 01:47:13 -0400

    just call it sync

--- mend-hub/scripts/setup.sh
+++ mend-hub/scripts/setup.sh
@@ -60,10 +60,10 @@ sudo -i -u fred-bot bash << EOF
 echo "Cloning repository..."
 git clone https://github.com/jacob-grahn/server-config.git ~/server-config
 
-# Set up cron job to run sync-from-repo.sh every 10 minutes
+# Set up cron job to run sync.sh every 10 minutes
 echo "Setting up cron job..."
-(crontab -l 2>/dev/null; echo "*/10 * * * * /home/fred-bot/server-config/scripts/sync-from-repo.sh") | crontab -
-(crontab -l 2>/dev/null; echo "@reboot /home/fred-bot/server-config/scripts/sync-from-repo.sh") | crontab -
+(crontab -l 2>/dev/null; echo "*/10 * * * * /home/fred-bot/server-config/scripts/sync.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /home/fred-bot/server-config/scripts/sync.sh") | crontab -
 EOF
 
 # Set up a root cron job to restart the VM once per month
