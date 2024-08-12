f892d526e9198faf51c39aa7d71b21fceb9a2de2
Jacob Grahn <jacob@grahn.io>
2024-08-12 17:03:39 -0400

    fix dir path

--- mend-hub/monitoring/sync.sh
+++ mend-hub/monitoring/sync.sh
@@ -4,7 +4,7 @@ set -e # Exit script if a command errors
 
 # Get the directory where the script is located
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd $SCRIPT_DIR/..
+cd $SCRIPT_DIR
 
 # Run services
 docker compose \
