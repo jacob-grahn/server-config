09dd7758688ad6e61890e5ad807d00a3557e508d
Jacob Grahn <jacob@grahn.io>
2024-08-12 14:26:36 -0400

    add cadvisor

---
+++ mend-hub/monitoring/sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e # Exit script if a command errors
+
+# Get the directory where the script is located
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd $SCRIPT_DIR/..
+
+# Run services
+docker compose \
+  -f monitoring.yaml \
+  -f monitoring.secrets.yaml \
+  --project-name monitoring \
+  up \
+  -d \
+  --remove-orphans
