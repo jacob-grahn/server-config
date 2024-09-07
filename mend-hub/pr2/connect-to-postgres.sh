0e96dd1aadfed3481086b80a2f55e624c4c7fe2a
Jacob Grahn <jacob@grahn.io>
2024-09-07 12:35:12 -0400

    script to log into mysql

---
+++ mend-hub/pr2/connect-to-postgres.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit script if a command errors
+set -e
+
+# Get the directory where the script is located
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Fetch password
+mysql_pass=$(yq -r -e .services.pr2-mysql.environment.MYSQL_ROOT_PASSWORD $SCRIPT_DIR/pr2.secrets.yaml)
+
+# Connect to mysql
+docker run -it --network pr2-backend --rm -e MYSQL_ROOT_PASSWORD=$mysql_pass mysql mysql -h pr2-mysql -u root
