143f6828b6cb842926a01403ca62985d9133fdf4
Jacob Grahn <jacob@grahn.io>
2024-09-11 02:08:39 -0400

    add pr2 http server

---
+++ mend-hub/pr2/render.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Go to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Inject image tags for pr4
+export HTTP_TAG=$(../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-http' '.*-main-.*')
+cat pr2.template.yaml | envsubst > pr2.rendered.yaml
