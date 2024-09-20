a569f379867f2577a663212c43439911d5586b26
Jacob Grahn <jacob@grahn.io>
2024-09-20 19:40:34 -0400

    trying out fancy replica id script

--- mend-hub/pr2/render.sh
+++ mend-hub/pr2/render.sh
@@ -5,4 +5,5 @@ cd "$(dirname "$0")"
 
 # Inject image tags for pr4
 export HTTP_TAG=$(../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-http' '.*-main-.*')
+export MULTI_TAG=$(../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-multi' '.*-main-.*')
 cat pr2.template.yaml | envsubst > pr2.rendered.yaml
