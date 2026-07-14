c91c5143e884fc037f995d92c76a1b00af4ba925
Jacob Grahn <jacob@grahn.io>
2026-07-14 02:04:36 -0400

    Add gameserver ingress routes and parameterize client tag

--- mend-hub/pr2/render.sh
+++ mend-hub/pr2/render.sh
@@ -4,6 +4,7 @@
 cd "$(dirname "$0")"
 
 # Inject image tags
+export CLIENT_TAG=$(../../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-client' '^[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9a-f]+$')
 export HTTP_TAG=$(../../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-http' '.*-main-.*')
 export MULTI_TAG=$(../../scripts/fetch-ghcr-tag.sh 'ghcr.io/jacob-grahn/platform-racing-2-multi' '.*-main-.*')
 cat pr2.template.yaml | envsubst > pr2.rendered.yaml
